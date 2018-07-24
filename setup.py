0b9525747fa5664489d5a4ed9fbaf1ff407cea5a
monitor1379 <yy4f5da2@hotmail.com>
2018-07-24 21:28:49 +0800

    <other> Add WTFPL Lisence

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ding',
+    version='v0.1.0',
+    packages=['ding', 'ding.tests'],
+    url='http://github.com/monitor1379/ding',
+    license='WTFPL',
+    author='monitor1379',
+    author_email='yy4f5da2@hotmail.com',
+    description='Dingtalk Message Pulisher'
+)
